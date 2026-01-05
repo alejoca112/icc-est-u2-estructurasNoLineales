e14f49d4107f5478d92b2eeadf1a37d77db73d03
DianitaB <valeria10diana@gmail.com>
2026-01-05 10:28:31 -0500

    Pre,Pos,In,Size

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+
+import trees.IntTree;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Estudiante: Geovanny Cabrera");
+        runIntTree();
+
+    }
+    private static void runIntTree(){
+        IntTree tree = new IntTree();
+
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(3);
+        tree.insert(15);
+        System.out.println("PreOrder");
+        tree.preOrder();
+        System.out.println("\nPosOrder");
+        tree.posOrder();
+        System.out.println("\nInOrder");
+        tree.inOrder();
+        System.out.println("\nSize: ");
+        tree.size();
+        
     }
 }
--- src/trees/intTree.java
+++ src/trees/intTree.java
@@ -1,5 +1,82 @@
 package trees;
+import nodes.Node;
 
-public class intTree {
-    
+public class IntTree {
+    private Node<Integer> root;
+    private int size;
+
+    public IntTree() {
+        this.root = null;
+        this.size = 0;
+    }
+
+    public void insert(int value) {
+        root = insertRecursive(root, value);
+        size++;
+    }
+
+    private Node<Integer> insertRecursive(Node<Integer> current, int value) {
+        if (current == null) {
+            return new Node<>(value);
+        }
+
+        //Insertar Nodos a la izquierda y a la derecha
+        if (value < current.getValue()) {
+            current.setLeft(insertRecursive(current.getLeft(), value));
+        //mandar else if porque no se puede insertar nodos duplicados
+        } else if(value > current.getValue()){
+            current.setRight(insertRecursive(current.getRight(), value));
+        }
+        return current;
+    }
+
+    public void preOrder(){
+        preOrderRecursive(root);
+    }
+
+    private void preOrderRecursive(Node<Integer> node){
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.getValue() + " ");
+        preOrderRecursive(node.getLeft());
+        preOrderRecursive(node.getRight());
+    }
+
+    public void posOrder(){
+        posOrderRecursive(root);
+    }
+
+    private void posOrderRecursive(Node<Integer> node){
+        if (node == null) {
+            return;
+        }
+        posOrderRecursive(node.getLeft());
+        posOrderRecursive(node.getRight());
+        System.out.print(node.getValue() + " ");
+    }
+
+    public void inOrder(){
+        inOrderRecursive(root);
+    }
+
+    private void inOrderRecursive(Node<Integer> node){
+        if (node == null) {
+            return;
+        }
+        inOrderRecursive(node.getLeft());
+        System.out.print(node.getValue() + " ");
+        inOrderRecursive(node.getRight());
+    }
+
+    public int size(){
+        return sizeRecursive(root);
+    }
+
+    private int sizeRecursive(Node<Integer> node){
+        if (node == null) {
+            return 0;
+        }
+        return size;
+    }
 }
